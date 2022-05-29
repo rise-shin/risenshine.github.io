0ee21cfc6ce791b07d194b8c3d30405cf408bac4
rise-shin <106480877+rise-shin@users.noreply.github.com>
2022-05-29 20:33:09 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const toggleButton=document.getElementsByClassName('toggle_button')[0];
-const navLinks=document.getElementsByClassName('nav_links')[0];
-toggleButton.addEventListener('click', () => {
-	navLinks.classList.toggle('hide');
-});
